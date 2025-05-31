7a5a9a92e65d6feb4e32050c58e8207ef25eb7dd
Som Kunar <hackerkunar@gmail.com>
2025-05-31 17:28:57 +0530

    Update ExtentReport.java

--- src/main/java/com/test/ReportUtils/ExtentReport.java
+++ src/main/java/com/test/ReportUtils/ExtentReport.java
@@ -3,10 +3,6 @@ package com.test.ReportUtils;
 import com.aventstack.extentreports.*;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.io.FileHandler;
 import org.testng.IReporter;
 import org.testng.IResultMap;
 import org.testng.ISuite;
@@ -15,8 +11,6 @@ import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.xml.XmlSuite;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -24,16 +18,13 @@ import java.util.Map;
 
 public class ExtentReport implements IReporter {
     private ExtentReports extent;
-    private static WebDriver driver;
 
-   
-    public static void setDriver(WebDriver driver2) {
-    	driver=driver2;
+    public ExtentReport() {
+
     }
 
     @Override
     public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
-      
         ExtentSparkReporter sparkReporter = new ExtentSparkReporter(outputDirectory + "/ExtentReport.html");
         sparkReporter.config().setTheme(Theme.STANDARD);
         sparkReporter.config().setDocumentTitle("Automation Test Report");
@@ -42,15 +33,12 @@ public class ExtentReport implements IReporter {
         extent = new ExtentReports();
         extent.attachReporter(sparkReporter);
 
-       
-        extent.setSystemInfo("Tester", "Kshitiz Dhooria");
+        extent.setSystemInfo("Tester", "Som Kunar");
         extent.setSystemInfo("Environment", "QA");
         extent.setSystemInfo("Browser", "Chrome");
 
-       
         for (ISuite suite : suites) {
             Map<String, ISuiteResult> result = suite.getResults();
-
             for (ISuiteResult r : result.values()) {
                 ITestContext context = r.getTestContext();
                 buildTestNodes(context.getPassedTests(), Status.PASS);
@@ -58,7 +46,8 @@ public class ExtentReport implements IReporter {
                 buildTestNodes(context.getSkippedTests(), Status.SKIP);
             }
         }
-        extent.flush(); 
+
+        extent.flush();
     }
 
     private void buildTestNodes(IResultMap tests, Status status) {
@@ -75,15 +64,6 @@ public class ExtentReport implements IReporter {
 
                 if (result.getThrowable() != null) {
                     test.log(status, result.getThrowable());
-
-                   
-                    if (status == Status.FAIL) {
-                        String screenshotPath = captureScreenshot(result.getMethod().getMethodName());
-                        
-                        test.addScreenCaptureFromPath(screenshotPath);
-                        
-
-                    }
                 } else {
                     test.log(status, "Test " + status.toString().toLowerCase() + "ed");
                 }
@@ -95,27 +75,5 @@ public class ExtentReport implements IReporter {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(millis);
         return calendar.getTime();
-    }
-
-
-    private String captureScreenshot(String screenshotName) {
-        String filePath = System.getProperty("user.dir") + "/screenshots/" + screenshotName + ".png";
-
-        try {
-            File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-
-            File directory = new File(System.getProperty("user.dir") + "/screenshots/");
-            if (!directory.exists()) {
-                directory.mkdir();
-            }
-
-            FileHandler.copy(source, new File(filePath));
-            System.out.println("Screenshot saved: " + filePath);
-
-        } catch (IOException e) {
-            System.out.println("Exception while taking screenshot: " + e.getMessage());
-        }
-
-        return filePath;
     }
 }
