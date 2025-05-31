2fea868afd637b512e5a8afa1c4935d0ec30a864
Som Kunar <hackerkunar@gmail.com>
2025-05-31 17:24:43 +0530

    Update LoginTest.java

--- src/test/java/com/test/LoginTest/LoginTest.java
+++ src/test/java/com/test/LoginTest/LoginTest.java
@@ -1,27 +1,53 @@
 package com.test.LoginTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
-import com.test.Base.Base;
 import com.test.Login.Login;
+import com.test.Products.Products;
+import com.test.Base.Base;
+import com.test.Cart.Cart;
+import com.test.Checkout.Checkout;
+
+import java.time.Duration;
 
 public class LoginTest extends Base {
+    WebDriver driver;
     Login login;
+    Products products;
+    Cart cart;
+    Checkout checkout;
 
-    
-    
     @BeforeMethod
     public void setUp() {
-        login = new Login(); 
+
+        login = new Login(driver);
+        products = new Products(driver);
+        cart = new Cart(driver);
+        checkout = new Checkout(driver);
     }
 
     @Test
-    public void validatePageTitle() {
-        String actualTitle = login.getTitle();
-        System.out.println("Actual Title: " + actualTitle);
-        
-        Assert.assertTrue(actualTitle.contains("Amazon"), "not contain");
+    public void ValidLogin() {
+        login.login("standard_user", "secret_sauce");
+        Assert.assertTrue(driver.getTitle().contains("Swag Labs"));
+    }
+
+    @Test
+    public void EndCheckout() {
+        login.login("standard_user", "secret_sauce");
+        products.addFirstItemToCart();
+        products.goToCart();
+        cart.clickCheckout();
+        checkout.fillCheckoutInfo();
+        checkout.clickFinish();
+        Assert.assertTrue(checkout.MessageDisplayed());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 }
